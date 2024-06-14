1c224313bad98ffaaaa4a13030003c00c18f0264
Octavio <octavio_aso@hotmail.com>
2024-06-14 18:00:47 -0300

    some_changes

--- src/pages/Rates/index.jsx
+++ src/pages/Rates/index.jsx
@@ -3,23 +3,30 @@ import { Footer } from "../../components/Footer/index"
 import { Slider } from '../../components/Slider/index'
 import { GoogleCard } from '../../components/GoogleCard/index'
 import { ContactButton } from "../../components/ContactButton";
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
 
-import { Container, MainContent, GoogleContent } from "./styles";
+import { Container, MainContent, GoogleContent, Images } from "./styles";
 
 export function Rates() {
   return (
     <Container>
       <Header />
-
-      <MainContent>
         <h1>Avaliações dos pacientes</h1>
+      <MainContent>
         <GoogleContent>
           <GoogleCard className='GoogleCard'/>
           <Slider />
         </GoogleContent>
         <ContactButton className='ContactButton'/>
+        <Images>
+          <img src={img1} className="img1" alt="imagem da dra executando um procedimento" />
+          <img src={img2} className="img2" alt="imagem do consultório" />
+          <img src={img3} className="img3" alt="imagem do consultório" />
+        </Images>
       </MainContent>
-      <Footer className={Footer}/>
+      <Footer className='Footer'/>
     </Container>
   )
 }
--- src/pages/Rates/styles.js
+++ src/pages/Rates/styles.js
@@ -5,26 +5,43 @@ export const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-`
-
-export const MainContent = styled.div`
-  padding-top: 14rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1.6rem;
 
   > h1 {
+    padding-top: 14rem;
     display: flex;
     font-family: Poppins;
     color: ${({ theme }) => theme.COLORS.TEXT_STANDARD};
     justify-content: center;
+
+    @media ${breakpoints.mobile_up} {
+      font-size: 3.6rem;
+    }
+  }
+
+  .Footer {
+    z-index: 3;
+  }
+`
+
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+
+  @media ${breakpoints.desktop_up} {
+    flex-direction: row;
   }
 
   > .ContactButton {
     display: flex;
     margin: auto;
+    margin-bottom: 1.6rem;
 
-    @media ${breakpoints.tablet_up} {
+    @media ${breakpoints.mobile_up} {
+      margin-top: 8rem;
+    }
+
+    @media ${breakpoints.desktop_up} {
       display: none;
     }
   }
@@ -35,12 +52,55 @@ export const GoogleContent = styled.div`
   flex-direction: column;
   gap: 1.6rem;
 
+  @media ${breakpoints.mobile_up} {
+    gap: 8rem;
+  }
+
+  @media ${breakpoints.desktop_up} {
+    padding-top: 10rem;
+    padding-left: 14%;
+  }
+
   > .GoogleCard {
     display: flex;
     justify-content: center;
+    margin-top: 1.6rem;
 
-    @media ${breakpoints.tablet_up} {
+    @media ${breakpoints.desktop_up} {
       display: none;
+    }
+  }
+`
+
+export const Images = styled.div`
+  padding-left: 20rem;
+
+  @media ${breakpoints.desktop_only} {
+      display: none;
+    }
+
+  @media ${breakpoints.desktop_up} {
+    .img3 {
+      width: 32rem;
+      position: absolute;
+      right: 26%;
+      top: 24rem;
+      z-index: 1;
+    }
+
+    .img2 {
+      width: 22rem;
+      position: absolute;
+      right: 14%;
+      z-index: 2;
+    }
+
+    .img1 {
+      width: 28rem;
+      position: absolute;
+      right: 14%;
+      top: 48rem;
+      z-index: 0;
     }
   }
 `
